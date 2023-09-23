7611743519bffd81777c2d1ac24cbd74621d1d4b
Zug <zugangliu33@gmail.com>
2023-09-23 21:51:32 +0800

    add freemarker and MinIO dependency in article service

---
+++ absolute-leadnews-service/absolute-leadnews-article/src/main/java/com/absolute/article/mapper/ApArticleContentMapper.java
@@ -0,0 +1,9 @@
+package com.absolute.article.mapper;
+
+import com.absolute.model.article.pojos.ApArticleContent;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ApArticleContentMapper extends BaseMapper<ApArticleContent> {
+}
